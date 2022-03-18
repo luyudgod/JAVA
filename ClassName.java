aa694efa2c93987f4c9446dd088fccdf76e577b1
luyudgod <51063730+luyudgod@users.noreply.github.com>
2022-03-18 17:08:44 +0800

    Add files via upload

---
+++ ClassName.java
@@ -0,0 +1,10 @@
+public class ClassName{
+    private boolean myFlag;
+static final double weeks = 9.5;
+protected static final int BOXWIDTH = 42;
+public static void main(String[] arguments){
+    //访问控制 修饰符 公共类下面的公共主函数
+}
+}
+
+
